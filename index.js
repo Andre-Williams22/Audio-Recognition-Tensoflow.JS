75058b29b827108b9ae309f43a9083b189af1ec3
Andre <awjrs22@gmail.com>
2019-09-07 09:33:47 -0700

    comments the training model

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const INPUT_SHAPE =[NUM_FRAMES, 232, 1];
+let model;
+
 // one frame is 23 ms
 const NUM_FRAMES = 3;
 let examples = []
@@ -14,8 +17,9 @@ function collect(label) {
         return;
     }
     recognizer.listen(async ({spectrogram: {frameSize, data}}) => {
-       // Since we want to use short sounds instead of words to control the slider, we are taking into consideration only the last 3 frames (~70ms):
+//Since we want to use short sounds instead of words to control the slider, we are taking into consideration only the last 3 frames (~70ms):
         let vals = normalize(data.subarray(-frameSize * NUM_FRAMES));
+        //Finally, each training example will have 2 fields:
         examples.push({vals, label});
         document.querySelector('#console').textContent = `${examples.length} examples collected`;
     }, {
@@ -33,6 +37,75 @@ function collect(label) {
     }
 }
 
+// train() trains the model using the collected data.
+
+// The training goes 10 times (epochs) over the data using a batch size of 16 (processing 16 examples at a time) 
+// and shows the current accuracy in the UI
+async function train() {
+    toggleButtons(false);
+    const ys = tf.oneHot(examples.map(e => e.label), 3);
+    const xsShape = [examples.length, ...INPUT_SHAPE];
+    const xs = tf.tensor(flatten(examples.map(e => e.vals)), xsShape);
+   
+    await model.fit(xs, ys, {
+      batchSize: 16,
+      epochs: 10,
+      callbacks: {
+        onEpochEnd: (epoch, logs) => {
+          document.querySelector('#console').textContent =
+              `Accuracy: ${(logs.acc * 100).toFixed(1)}% Epoch: ${epoch + 1}`;
+        }
+      }
+    });
+    tf.dispose([xs, ys]);
+    toggleButtons(true);
+   }
+
+// BuildModel() defines the model architecture   
+   function buildModel() {
+    // Model Architecture
+    //The model has 4 layers: a convolutional layer that processes the audio data (represented as a spectrogram), 
+    // a max pool layer, a flatten layer, and a dense layer that maps to the 3 actions:
+    model = tf.sequential();
+    model.add(tf.layers.depthwiseConv2d({
+      depthMultiplier: 8,
+      kernelSize: [NUM_FRAMES, 3],
+      activation: 'relu', // uses relu activation function
+      inputShape: INPUT_SHAPE
+    }));
+    model.add(tf.layers.maxPooling2d({poolSize: [1, 2], strides: [2, 2]}));
+    model.add(tf.layers.flatten());
+    model.add(tf.layers.dense({units: 3, activation: 'softmax'}));
+// The input shape of the model is [NUM_FRAMES, 232, 1] where each frame is 23ms of audio containing 232 numbers that 
+//correspond to different frequencies (232 was chosen because it is the amount of frequency buckets needed to capture the human voice).
+// we are using samples that are 3 frames long (~70ms samples) since we are making sounds instead of 
+//speaking whole words to control the slider
+
+//compile our model to get it ready for training
+    const optimizer = tf.train.adam(0.01);
+    model.compile({
+      optimizer,
+      loss: 'categoricalCrossentropy',
+      metrics: ['accuracy']
+    });
+   }
+   // use Adam Optimizer a common optimizer used in deep learning, and `categoricalCrossEntropy` for loss, the standard loss function used for 
+   //classification. In short, it measures how far the predicted probabilities (one probability per class) are from having 100% probability 
+   //in the true class, and 0% probability for all the other classes. We also provide accuracy as a metric to monitor, which will give us the 
+   //percentage of examples the model gets correct after each epoch of training.
+   
+   function toggleButtons(enable) {
+    document.querySelectorAll('button').forEach(b => b.disabled = !enable);
+   }
+   
+   function flatten(tensors) {
+    const size = tensors[0].length;
+    const result = new Float32Array(tensors.length * size);
+    tensors.forEach((arr, i) => result.set(arr, i * size));
+
+    return result;
+   }
+/*
 let recognizer; 
 
 function predictWord() {
@@ -45,10 +118,12 @@ function predictWord() {
         scores.sort((s1, s2) => s2.score - s1.score);
         document.querySelector("#console").textContent = scores[0].word;
     }, {probabilityThreshold: 0.75});} // 0.75 means the model will fire when it feels like it's more than 75% confident
-
+*/
 async function app() {
     recognizer = speechCommands.create('BROWSER.FFT');
     await recognizer.ensureModeLoaded();
+    // builds the model
+    buildModel();
 
 }
 
